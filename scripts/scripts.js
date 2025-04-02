fb7bac136e8ddcda4a37026ec58921533c7c4c34
Markus Haack <mhaack@adobe.com>
2025-04-02 17:43:36 +0200

    fix: sections

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -73,22 +73,22 @@ function decorateSections(main) {
   main.querySelectorAll(':scope > div').forEach((section) => {
     const wrappers = [];
     let defaultContent = false;
-
     [...section.children].forEach((e) => {
       if (e.classList.contains('richtext')) {
-        e.className = 'default-content-wrapper';
-        wrappers.push(e);
-      } else if (e.getAttribute('data-aue-type') === 'media' || e.tagName === 'DIV' || !defaultContent) {
-        const wrapper = document.createElement('div');
-        if (e.tagName !== 'DIV' || e.getAttribute('data-aue-type') === 'media') {
-          wrapper.classList.add('default-content-wrapper');
+        e.removeAttribute('class');
+        if (!defaultContent) {
+          const wrapper = document.createElement('div');
+          wrapper.classList.add('default-content-wrapper')
+          wrappers.push(wrapper);
+          defaultContent = true;
         }
+      } else if (e.tagName === 'DIV' || !defaultContent) {
+        const wrapper = document.createElement('div');
         wrappers.push(wrapper);
-        wrapper.append(e);
         defaultContent = e.tagName !== 'DIV';
-      } else {
-        wrappers[wrappers.length - 1].append(e);
+        if (defaultContent) wrapper.classList.add('default-content-wrapper');
       }
+      wrappers[wrappers.length - 1].append(e);
     });
 
     // Add wrapped content back
