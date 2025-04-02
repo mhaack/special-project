4483e26986513b596737516312b683d3ff02ad0b
Markus Haack <mhaack@adobe.com>
2025-04-02 14:39:53 +0200

    fix: sections

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,13 +4,15 @@ import {
   loadFooter,
   decorateButtons,
   decorateIcons,
-  decorateSections,
   decorateBlocks,
   decorateTemplateAndTheme,
   waitForFirstImage,
   loadSection,
   loadSections,
   loadCSS,
+  readBlockConfig,
+  toClassName,
+  toCamelCase,
 } from './aem.js';
 
 /**
@@ -61,6 +63,61 @@ function buildHeroBlock(main) {
     section.append(buildBlock('hero', { elems: [picture, h1] }));
     main.prepend(section);
   }
+}
+
+/**
+ * Decorates all sections in a container element.
+ * @param {Element} main The container element
+ */
+function decorateSections(main) {
+  main.querySelectorAll(':scope > div').forEach((section) => {
+    const wrappers = [];
+    let defaultContent = false;
+
+    [...section.children].forEach((e) => {
+      if (e.classList.contains('richtext')) {
+        e.className = 'default-content-wrapper';
+        wrappers.push(e);
+      } else if (e.getAttribute('data-aue-type') === 'media') {
+        const wrapper = document.createElement('div');
+        wrapper.classList.add('default-content-wrapper');
+        wrappers.push(wrapper);
+        wrapper.append(e);
+      } else if (e.tagName === 'DIV' || !defaultContent) {
+        const wrapper = document.createElement('div');
+        wrappers.push(wrapper);
+        defaultContent = e.tagName !== 'DIV';
+        if (defaultContent) wrapper.classList.add('default-content-wrapper');
+        wrapper.append(e);
+      } else {
+        wrappers[wrappers.length - 1].append(e);
+      }
+    });
+
+    // Add wrapped content back
+    wrappers.forEach((wrapper) => section.append(wrapper));
+    section.classList.add('section');
+    section.dataset.sectionStatus = 'initialized';
+    section.style.display = 'none';
+
+    // Process section metadata
+    const sectionMeta = section.querySelector('div.section-metadata');
+    if (sectionMeta) {
+      const meta = readBlockConfig(sectionMeta);
+      Object.keys(meta).forEach((key) => {
+        if (key === 'style') {
+          const styles = meta.style
+            .split(',')
+            .filter((style) => style)
+            .map((style) => toClassName(style.trim()));
+          styles.forEach((style) => section.classList.add(style));
+        } else {
+          section.dataset[toCamelCase(key)] = meta[key];
+        }
+      });
+      sectionMeta.parentNode.remove();
+    }
+  });
 }
 
 /**
